cd953769aea5c8a8c5c68304e13ec3c8b11a98fc
AngomRanjan <angomranjan@gmail.com>
2022-05-10 02:57:37 +0530

    Add Functionality To Add,Multiply,Subtract and Divide Buttons

--- calci.js
+++ calci.js
@@ -17,22 +17,46 @@ function multiply(num1,num2) {
 }
 
 function operate(num1,num2,operator){
-  alert("you click"+" '"+operator.name+"'");
-  display.textContent= operator (num1,num2);
+  if(keyMode==="number1") {
+    n1=Number(display.textContent);
+  }
+  else if (keyMode==="number2") {
+    n2=Number(display.textContent);
+    n1= window[curOps] (n1,n2);
+    display.textContent=n1;
+    n2="";
+  }
+
+  curOps=operator;
+  keyMode='ops'
+  //display.textContent= operator (num1,num2);
 }
 
 function populate(num,s) {
-  (Number(display.textContent)===0) ? display.textContent=num:display.textContent=display.textContent + num;
+  if (keyMode==='ops') {
+    boolDecimal=false;
+    keyMode='number2';
+    display.textContent=0;
+  }
+  if (num==='.') {
+    (boolDecimal) ? display.textContent=display.textContent: display.textContent+=num;
+    boolDecimal=true;
+  }
+  else 
+    (Number(display.textContent)===0 && boolDecimal===false) ? display.textContent=num:display.textContent+=num;
 }
 
-const n1=6;
-const n2=2;
+let n1=6;
+let n2=2;
 const display=document.getElementById('display');
+let boolDecimal=false;
+let keyMode='number1';
+let curOps=''
 
 let tempArray = document.querySelectorAll("button");
 
 tempArray.forEach(function(numKeys) {
     numKeys.addEventListener("click", function() {
-        (numKeys.className==="numKeys") ? populate(numKeys.id,0):operate(n1,n2,window[numKeys.id]);
+        (numKeys.className==="numKeys") ? populate(numKeys.id,0):operate(n1,n2,numKeys.id);
     });
 });
